a8a549288ca986508fcdc91e79222953fdeefa3e
Nils Windisch <mail@nilswindisch.de>
2016-11-08 13:13:38 +0100

    Fix function "mark publication" #13 - fix on publication page

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -1,60 +1,56 @@
 $(function () {
   $('.mark').click(function(evt) {
-    console.log('click mark button');
-        evt.preventDefault();
-        var a = $(this);
-        var marked = a.data('marked');
-        if (marked === 0) {
-            console.log('not marked');
-            $.post('/mark/'+a.data('id'), function(res) {
-                console.log('post doc');
-                $('.total-marked').text(res.total);
-                a.data('marked', 1);
-                // a.children('span').removeClass('fa-square-o');
-                // a.children('span').addClass('fa-check-square-o');
-            }, 'json');
+    evt.preventDefault();
+    var a = $(this);
+    var marked = a.data('marked');
+    var csrfToken = $("meta[name='csrf_token']").attr("content");
+    if (marked == 0) {
+      $.ajax({
+        type: 'POST',
+        url: '/mark/'+a.data('id'),
+        headers: {
+          "X-CSRF-Token": csrfToken
+        },
+        dataType: 'json',
+        success: function(res) {
+          $('.total-marked').text(res.total);
+          a.data('marked', 1);
+          a.html('Marked');
         }
-        else {
-            console.log('marked');
-            $.post('/mark/'+a.data('id')+'?x-tunneled-method=DELETE', function(res) {
-                $('.total-marked').text(res.total);
-                a.data('marked', 0);
-                // a.children('span').removeClass('fa-check-square-o');
-                // a.children('span').addClass('fa-square-o');
-            }, 'json');
-            if(a.attr('id') && /clickme_(\d{1,})/i.test(a.attr('id'))){
-                var indexes = a.attr('id').match(/clickme_\d{1,}/i);
-                indexes[0] = indexes[0].replace(/clickme_/,"");
-                $('#fade_' + indexes[0]).fadeOut('slow', function() {});
-            }
+      });
+    } else {
+      $.ajax({
+        type: 'POST',
+        url: '/mark/'+a.data('id')+'?x-tunneled-method=DELETE',
+        headers: {
+          "X-CSRF-Token": csrfToken
+        },
+        dataType: 'json',
+        success: function(res) {
+          $('.total-marked').text(res.total);
+          a.data('marked', 0);
+          a.html('Not marked');
         }
-    });
+      });
+      if(a.attr('id') && /clickme_(\d{1,})/i.test(a.attr('id'))){
+        var indexes = a.attr('id').match(/clickme_\d{1,}/i);
+        indexes[0] = indexes[0].replace(/clickme_/,"");
+        $('#fade_' + indexes[0]).fadeOut('slow', function() {});
+      }
+    }
+  });
 });
 
-/**
- * tho following bit seems to be outdated an replaced
- * originally from librecat pre layer age
- */
-// $( document ).ready(function() {
-//     $.post('/marked_total', function(res) {
-//         $('.total-marked').text(res.total);
-//     });
-// });
-
-/**
- * the following bit replaces the line above
- * originally from librecat post layer age
- */
 $(document).ready(function() {
-    var totalMarked = $('.total-marked');
-    if (totalMarked.length) {
-        $.ajax({
-            type: 'GET',
-            url: '/marked_total',
-            dataType: 'json',
-            success: function(res) {
-               totalMarked.text(res.total);
-            }
-        });
-    }
+  var totalMarked = $('.total-marked');
+  if (totalMarked.length) {
+    $.ajax({
+      type: 'GET',
+      url: '/marked_total',
+      dataType: 'json',
+      success: function(res) {
+       totalMarked.text(res.total);
+      }
+    });
+  }
 });
